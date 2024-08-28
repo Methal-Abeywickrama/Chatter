0eb8a51af480f1430ab42970a2e1453414f66cb2
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-08-28 11:55:34 +0530

    Set up basic models and associations
    The four basic associations are post, user, profile and comment
    Comment model is polymorphic as commentable
    User has a self join for followers and followees
    Profile and post belong to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_one :profile, dependent: :destroy
+  has_many :follwers, class_name: "User", foreign_key: "followee"
+  belongs_to :followee, class_name: "User"
 end
