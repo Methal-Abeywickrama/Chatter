db132e1cc98ab98928743ebd7f1deb8e5914c5da
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-08-28 18:59:04 +0530

    Change the follower followee class
    Followers and followees are now based on the profile model instead of
    the user model, since its safer to make the profiles public.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_many :follwers, class_name: "User", foreign_key: "followee_id"
+  belongs_to :followee, class_name: "User", optional: true
 end
