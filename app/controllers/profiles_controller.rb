d861293f1e85de03e15f61be9a3052e0f2fe3f60
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-08-29 09:58:33 +0530

    Change profile from singular resource to standard
    Two custom routes -> my_profile and edit_my_profile have been added to
    be able to reference show and edit paths without the necessity of ids.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,7 +15,7 @@ class ProfilesController < ApplicationController
 
   def update
     if current_user.profile.update(profile_params)
-      redirect_to profile_path, notice: "Profile edited successfully"
+      redirect_to my_profile_path, notice: "Profile edited successfully"
     else 
       render :edit, status: :unprocessable_entity
     end
@@ -24,7 +24,7 @@ class ProfilesController < ApplicationController
   def create
     @profile = current_user.build_profile(profile_params)
     if @profile.save! 
-      redirect_to profile_path, notice: "Profile created successfully"
+      redirect_to my_profile_path, notice: "Profile created successfully"
     else 
       render :new, status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :posts
-  resource :profile
+  resources :profiles
   root "posts#index"
+  # Custom route to show the current user's profile
+  get 'my_profile', to: 'profiles#show', as: 'my_profile'
+  get 'edit_my_profile', to: 'profiles#edit', as: 'edit_my_profile'
   # Defines the root path route ("/")
   # root "posts#index"
 end
